43e1c305cc7b886421f903873235b115e39e51a6
Preetish Niket <niketpreetish@gmail.com>
2020-07-09 14:14:38 +0530

    Patterns

--- src/Basic/Pattern27.java
+++ src/Basic/Pattern27.java
@@ -1,9 +1,13 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class Pattern27 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
 
 	}
 
