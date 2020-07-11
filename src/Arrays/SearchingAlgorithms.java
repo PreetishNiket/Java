ff447281bf2ed9f33488486438c73525176c1161
Preetish Niket <niketpreetish@gmail.com>
2020-07-11 14:22:18 +0530

    BinarySearch

--- src/Arrays/SearchingAlgorithms.java
+++ src/Arrays/SearchingAlgorithms.java
@@ -1,5 +1,6 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SearchingAlgorithms {
@@ -12,15 +13,35 @@ public class SearchingAlgorithms {
 		for(int i=0;i<a.length;i++) {
 			a[i]=scan.nextInt();
 		}
+		Arrays.sort(a);
 		int element=scan.nextInt();
 		scan.close();
-		System.out.println(linearSearch(a, element));
+		//System.out.println(linearSearch(a, element));
+		System.out.println(binarySearch(a, element));
 	}
 	public static int linearSearch(int a[],int element)
 	{
 		for(int i=0;i<a.length;i++) {
 			if(a[i]==element) {
 				return i;
+			}
+		}
+		return -1;
+	}
+	public static int binarySearch(int a[],int element){
+		
+		int low=0;
+		int high=a.length-1;
+		while(low<=high) {
+			int mid=(low+high)/2;
+			if(element>a[mid]) {
+				low=mid+1;
+			}
+			else if(element<a[mid]) {
+				high=mid-1;
+			}
+			else {
+				return mid;
 			}
 		}
 		return -1;
