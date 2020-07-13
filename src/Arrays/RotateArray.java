794253d8ef2d012ffb959e9fd56a3db081c79676
Preetish Niket <niketpreetish@gmail.com>
2020-07-13 17:54:54 +0530

    Rotate Array

---
+++ src/Arrays/RotateArray.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class RotateArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+			int n=scan.nextInt();
+			int r=scan.nextInt();
+			int a[]=new int[n];
+			for(int i=0;i<a.length;i++) {
+				a[i]=scan.nextInt();
+			}
+
+			int temp;
+			for(int i=0;i<r;i++) {
+				temp=a[0];
+				for(int j=0;j<a.length-1;j++) {
+					a[j]=a[j+1];
+				}
+				a[a.length-1]=temp;
+			}
+			for(int i=0;i<a.length;i++) {
+				System.out.print(a[i]+",");
+			}
+			scan.close();
+	}
+
+}
