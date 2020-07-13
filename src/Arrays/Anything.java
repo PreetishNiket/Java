9782b9c3d4b9594dd99009e9214bb9cc28df457c
Preetish Niket <niketpreetish@gmail.com>
2020-07-14 02:16:49 +0530

    Arrays

--- src/Arrays/Anything.java
+++ src/Arrays/Anything.java
@@ -1,9 +1,12 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Anything {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sca=new Scanner(System.in);
 
 	}
 
