4cbb3460fc9eaa127fa08e423dd0a62a224d95d1
Preetish Niket <niketpreetish@gmail.com>
2020-07-14 14:53:36 +0530

    RotateArrayEfficeintway

--- src/Arrays/RotateArray.java
+++ src/Arrays/RotateArray.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class RotateArray {
 
-	public static void main(String[] args) {
+	public static void m(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
+		int t=scan.nextInt();
+		while(t>0) {
 			int n=scan.nextInt();
 			int k=scan.nextInt();
 			int a[]=new int[n];
@@ -25,7 +27,32 @@ public class RotateArray {
 			for(int i=0;i<a.length;i++) {
 				System.out.print(a[i]+",");
 			}
-			scan.close();
+			t--;
+		}
+		scan.close();
 	}
-
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int test = in.nextInt();
+        while(test-->0)
+        {
+            int N = in.nextInt();
+            int D = in.nextInt();
+            int a[]= new int[N];
+            for(int i=0; i<N; i++)
+            {
+               a[i] = in.nextInt();
+            }
+            StringBuffer s = new StringBuffer();
+            for(int i=D; i<N; i++ )
+            {
+                s.append(a[i]+" ");
+            }
+            for(int i=0;i<D;i++)
+            {
+                s.append(a[i]+" ");
+            }
+            System.out.println(s);
+        }
+	}
 }
