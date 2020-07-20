f34248a4a01e8adcb8a1e40f06a63cb906aa33fa
Preetish Niket <niketpreetish@gmail.com>
2020-07-20 11:47:34 +0530

    RecursionDemo

---
+++ src/Recursion/PrintInc.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintInc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int n= scan.nextInt();
+		Pdi(n);
+		scan.close();
+	}
+
+	private static void Pdi(int n) {
+		if(n==0) {
+			return;
+		}
+		Pdi(n-1);
+		System.out.println(n);
+	}
+
+}
