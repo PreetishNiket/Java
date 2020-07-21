50c5cbd6bb81b782a7a14441de0ca459131d8340
Preetish Niket <niketpreetish@gmail.com>
2020-07-21 16:20:53 +0530

    FirstIndexOuccurenceArrayRecursive

--- src/Recursion/FirstIndexOuccurenceArrayRecursive.java
+++ src/Recursion/FirstIndexOuccurenceArrayRecursive.java
@@ -7,25 +7,25 @@ public class FirstIndexOuccurenceArrayRecursive {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		int n=scan.nextInt();
-		int []a=new int[n];
-		for(int i=0;i<n;i++) {
-			a[i]=scan.nextInt();
+		int n = scan.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = scan.nextInt();
 		}
-		int i=0;
-		int element=scan.nextInt();
-		System.out.println(firstIndex(a,i,element));
+		int i = 0;
+		int element = scan.nextInt();
+		System.out.println(firstIndex(a, i, element));
 		scan.close();
 	}
 
 	private static int firstIndex(int[] a, int i, int element) {
-		if(i==a.length) {
+		if (i == a.length) {
 			return -1;
 		}
-		if(a[i]==element) {
+		if (a[i] == element) {
 			return i;
-		}else {
-			int restAns=firstIndex(a, i+1, element);
+		} else {
+			int restAns = firstIndex(a, i + 1, element);
 			return restAns;
 		}
 	}
