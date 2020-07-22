4b252be6270d5fb13f002ede65c17be6e44775ad
Preetish Niket <niketpreetish@gmail.com>
2020-07-22 17:52:14 +0530

    SelectionSort

--- src/Algo/SortingAlgorithms.java
+++ src/Algo/SortingAlgorithms.java
@@ -12,26 +12,27 @@ public class SortingAlgorithms {
 		for (int i = 0; i < a.length; i++) {
 			a[i] = scan.nextInt();
 		}
-		bubbleSort(a);
+		// bubbleSort(a);
+		selectionSort(a);
 		display(a);
 		scan.close();
 	}
 
 	private static void display(int[] a) {
-		for(int i=0;i<a.length;i++) {
-			System.out.print(a[i]+" ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
 		}
-	
+
 	}
 
 	private static void bubbleSort(int[] a) {
-			for(int i=1;i<a.length;i++) {
-				for(int j=0;j<a.length-i;j++) {
-					if(a[j]>a[j+1]) {
-						swap(a, j, j+1);
-					}
+		for (int i = 1; i < a.length; i++) {
+			for (int j = 0; j < a.length - i; j++) {
+				if (a[j] > a[j + 1]) {
+					swap(a, j, j + 1);
 				}
 			}
+		}
 	}
 
 	private static void swap(int[] a, int i, int j) {
@@ -40,4 +41,17 @@ public class SortingAlgorithms {
 		a[j] = temp;
 	}
 
+	private static void selectionSort(int[] a) {
+		for (int i = 0; i < a.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[j] < a[min]) {
+					min = j;
+				}
+			}
+			if (min != i) {
+				swap(a, i, min);
+			}
+		}
+	}
 }
