be302bdfa4db0110e7c415b33cd1eefa0fbd1a9e
Preetish Niket <niketpreetish@gmail.com>
2020-07-24 18:22:38 +0530

    QuickSortAlgorithm

--- src/Algo/QuickSortAlgorithm.java
+++ src/Algo/QuickSortAlgorithm.java
@@ -14,34 +14,34 @@ public class QuickSortAlgorithm {
 		}
 		int lo = 0;
 		int hi = a.length - 1;
-		quickSort(a,lo,hi);
+		quickSort(a, lo, hi);
 	}
 
 	private static void quickSort(int[] a, int lo, int hi) {
-		//base case
-		
-		int mid =(lo+hi)/2;
-		int left=lo;
-		int right=hi;
-		int pivot=a[mid];
-		while(left<right) {
-			while(a[left]<pivot) {
+		// base case
+
+		int mid = (lo + hi) / 2;
+		int left = lo;
+		int right = hi;
+		int pivot = a[mid];
+		while (left < right) {
+			while (a[left] < pivot) {
 				left++;
 			}
-			while(a[right]>pivot) {
+			while (a[right] > pivot) {
 				right--;
 			}
-			if(left<=right) {
-				//swap
-				int temp=a[left];
-				a[left]=a[right];
-				a[right]=temp;
+			if (left <= right) {
+				// swap
+				int temp = a[left];
+				a[left] = a[right];
+				a[right] = temp;
 				left++;
 				right--;
 			}
 		}
-		//smaller problem
-		
+		// smaller problem
+		quickSort(a, lo, right);
+		quickSort(a, left, hi);
 	}
-
 }
