3ec7b25602596d5cb260a5acd9368961e6f8d391
Preetish Niket <niketpreetish@gmail.com>
2020-08-11 17:03:45 +0530

    HackerRankSolution

---
+++ src/Strings/SubStringComparison.java
@@ -0,0 +1,29 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class SubStringComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		int k=scan.nextInt();
+		String smallest="";
+		String largest ="";
+		smallest=largest=s.substring(0,k);
+		
+		for(int i=1;i<s.length()-k+1;i++) {
+			String substr=s.substring(i,i+k);
+			if(smallest.compareTo(substr)>0) {
+				smallest=substr;
+			}
+			if(largest.compareTo(substr)<0) {
+				largest=substr;
+			}
+		}
+		scan.close();
+		System.out.println(smallest+" "+largest);
+	}
+
+}
