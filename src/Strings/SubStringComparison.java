621f30f8ff6c8af505831859713f179a23368f7b
Preetish Niket <niketpreetish@gmail.com>
2020-08-11 17:07:12 +0530

    HackerRankSolution

--- src/Strings/SubStringComparison.java
+++ src/Strings/SubStringComparison.java
@@ -13,7 +13,7 @@ public class SubStringComparison {
 		String largest ="";
 		smallest=largest=s.substring(0,k);
 		
-		for(int i=1;i<s.length()-k+1;i++) {
+		for(int i=1;i<=s.length()-k;i++) {
 			String substr=s.substring(i,i+k);
 			if(smallest.compareTo(substr)>0) {
 				smallest=substr;
