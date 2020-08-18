bb8723f981f969368d7ffd2e887af3f294fac7db
Preetish Niket <niketpreetish@gmail.com>
2020-08-18 20:18:05 +0530

    Recursion

--- src/Recursion/PrintDecInc.java
+++ src/Recursion/PrintDecInc.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PrintDecInc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		Scanner scan = new Scanner(System.in);
 		int n=scan.nextInt();
 		Pdein(n);
--- src/Recursion/PrintInc.java
+++ src/Recursion/PrintInc.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PrintInc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner scan = new Scanner(System.in);
 		int n= scan.nextInt();
 		Pdi(n);
