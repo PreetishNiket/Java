b329c4fd26906bebe2af657886b7493633bf5bea
Preetish Niket <niketpreetish@gmail.com>
2020-09-20 13:16:52 +0530

    StacksImplementation

--- src/Stacks_Queue/StackusingArrays.java
+++ src/Stacks_Queue/StackusingArrays.java
@@ -17,5 +17,42 @@ public class StackusingArrays {
 		public int size() {
 			return this.top+1;
 		}
+		public boolean isEmpty() {
+			if(this.size()==0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		public int top() throws Exception {
+			if(this.size()==0) {
+				throw new Exception("Stack is Empty");
+			}
+			int rv =this.data[this.top];
+			return rv;
+		}
+		public void push(int value) throws Exception{
+			if(this.size()==this.data.length) {
+				throw new Exception("Stack is Full");
+			}
+			this.top++;
+			this.data[this.top]=value;
+		}
+		public int pop() throws Exception {
+			if(this.size()==0) {
+				throw new Exception("Stack is Empty");
+			}
+			int rv=this.data[this.top];
+			this.data[this.top]=0;
+			this.top--;
+			return rv;
+		}
+		public void display() {
+			for(int i=this.top;i>=0;i--) {
+				System.out.println(this.data[i]);
+			}
+			System.out.println("END");
+		}
 		
 }
