81848db2be70ea90ee2337535068703e64e71e2b
Preetish Niket <niketpreetish@gmail.com>
2020-09-20 13:29:37 +0530

    StackClient

--- src/Stacks_Queue/StackClient.java
+++ src/Stacks_Queue/StackClient.java
@@ -4,11 +4,19 @@ public class StackClient {
 
 	public static void main(String[] args) throws Exception {
 		StackusingArrays stack=new StackusingArrays(5);
-		for(int i=0;i<5;i++) {
+		for(int i=1;i<=5;i++) {
 			stack.push(i*10);
 			stack.display();
 		}
-
+		stack.pop();
+		stack.display();
+		System.out.println(stack.top());
+		if(stack.isEmpty()) {
+			throw new Exception("Stack is Empty");
+		}
+		else {
+			stack.display();
+		}
 	}
 
 }
