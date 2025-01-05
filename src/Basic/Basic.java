e3658f253fe732545ba4d88ab9a4fb938d4f1561
Preetish Niket <niketpreetish@gmail.com>
2025-01-05 19:04:51 +0530

    Revision for Java DSA

--- src/Basic/Basic.java
+++ src/Basic/Basic.java
@@ -1,38 +1,11 @@
 package Basic;
 
-import java.util.Scanner;
 
 public class Basic {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-//        System.out.println("================================");
-//        for(int i=0;i<3;i++)
-//        {
-//            String s1=sc.next();
-//            int x=sc.nextInt();
-//            //Complete this line
-//            System.out.printf("%-15s%03d\n",s1,x);
-//            
-//        }
         System.out.println("================================");
        
 	}
 
 }
-//Scanner scan = new Scanner(System.in);
-//int n=scan.nextInt();
-//Double d=scan.nextDouble();
-//String s=scan.nextLine();
-//if(n%2==1) {
-//	System.out.println("Weird");
-//}else {
-//	if(n>=6&&n<=20) {
-//	   System.out.println("Weird");
-//	}
-//	else {
-//		System.out.println("Not Weird");
-//	}
-//	
-//}
