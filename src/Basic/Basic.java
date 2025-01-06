c81c1b75094336485a7f62218215fee672998d96
Preetish Niket <niketpreetish@gmail.com>
2025-01-06 17:50:14 +0530

    I/O in Java

--- src/Basic/Basic.java
+++ src/Basic/Basic.java
@@ -1,11 +1,16 @@
 package Basic;
 
+import java.util.Scanner;
 
 public class Basic {
 
 	public static void main(String[] args) {
-        System.out.println("================================");
-       
+         
+		 Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter your name: ");
+	        String name = scanner.nextLine();
+	        System.out.println("Hello, " + name + "!");
+	        scanner.close();
+        
 	}
-
 }
